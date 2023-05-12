d93400c49347e4d061a71617d07f368983169907
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-05-12 17:27:19 +0200

    Scope and apply_on_cells have gone into jupyterlab_celltagsclasses

--- src/index.ts
+++ src/index.ts
@@ -26,44 +26,7 @@ import {
 
 import { md_get, md_set, md_unset, md_insert, md_remove, md_clean } from 'jupyterlab-celltagsclasses'
 
-/* the logic of applying a function on a set of cells */
-enum Scope {
-  All,        // run on all cells
-  Active,     // the active cell only
-  Multiple,   // the multiple selected if that is the case, the active cell otherwise
-}
-
-const apply_on_cells = (
-  notebookTracker: INotebookTracker,
-  scope: Scope,
-  to_apply: (cell: Cell) => void,
-) => {
-  const notebook = notebookTracker.currentWidget?.content
-  if (notebook === undefined) { return }
-
-  let actionCells
-  if (scope === Scope.All) {
-    actionCells = notebook.widgets.slice()
-  } else {
-    const activeCell = notebook.activeCell
-    if (activeCell === null) { return }
-
-    if (scope === Scope.Active) {
-      actionCells = [activeCell]
-    } else {
-      const { anchor, head } = notebook.getContiguousSelection()
-      // when only one cell is selected/active, both are null
-      if (anchor === null || head === null) {
-        actionCells = [activeCell]
-      } else {
-        actionCells = notebook.widgets.slice(anchor, head + 1)
-      }
-    }
-  }
-  // console.log(`apply_on_cells with scope=${scope} on ${actionCells.length} cells`)
-
-  actionCells.forEach(to_apply)
-}
+import { Scope, apply_on_cells } from 'jupyterlab-celltagsclasses'
 
 /*
 in order to have consistent behaviour between
@@ -132,7 +95,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   requires: [ICommandPalette, INotebookTracker],
   activate: (app: JupyterFrontEnd, palette: ICommandPalette, notebookTracker: INotebookTracker) => {
-    console.log('JupyterLab extension jupyterlab-tpt is activating')
+    console.log('extension jupyterlab-tpt is activating')
     // console.log('ICommandPalette', palette)
     // console.log('INotebookTracker', notebookTracker)
 
